336149d5ec20b15221287339c67d76b2027cdda1
Howon Lee <kck4156@kagamine-rin.com>
2026-04-13 20:24:53 +0900

    싱글 인스턴스 보장

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using Microsoft.Windows.AppLifecycle;
+using System;
+using System.Threading;
+
+namespace YellowInside;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main(string[] arguments)
+    {
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+
+        var isRedirect = DecideRedirection();
+        if (isRedirect) return;
+
+        Application.Start(callback =>
+        {
+            var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+            SynchronizationContext.SetSynchronizationContext(context);
+            _ = new App();
+        });
+    }
+
+    private static bool DecideRedirection()
+    {
+        var currentInstance = AppInstance.FindOrRegisterForKey("YellowInside_SingleInstance");
+
+        if (!currentInstance.IsCurrent)
+        {
+            var activationArguments = AppInstance.GetCurrent().GetActivatedEventArgs();
+            currentInstance.RedirectActivationToAsync(activationArguments).AsTask().Wait();
+            return true;
+        }
+
+        currentInstance.Activated += OnAppInstanceActivated;
+        return false;
+    }
+
+    private static void OnAppInstanceActivated(object sender, AppActivationArguments arguments)
+    {
+        App.ShowManageWindow();
+    }
+}
